4e093bb140fef0d0e109b20c8a3a700c4015e365
Nahum Carpio <nahum20x20@gmil.com>
2026-03-29 01:09:53 -0300

    Novas implementações

--- catalogo/js/data.js
+++ catalogo/js/data.js
@@ -3,8 +3,8 @@ export const categories = [
         title: "Épicos",
         items: [
             {
-                img: "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg", top10: true, badge: "Clássico", badgeColor: "red", progress: 0,
-                youtube: "https://www.youtube.com/watch?v=bLvqoHBptjg"
+                img: "https://cinema-em-cena.nyc3.cdn.digitaloceanspaces.com/reviews/1018/uRNgkJSkNBFbbn9fPsEjDIy8Sh3.jpg", top10: true, badge: "Clássico", badgeColor: "red", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=naQr0uTrH_s"
             },
             {
                 img: "https://aventurasnahistoria.com.br/wp-content/uploads/entretenimento/gladiador_2_VvnGVes.jpg", progress: 0,
@@ -23,10 +23,10 @@ export const categories = [
     {
         title: "Séries",
         items: [
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://m.media-amazon.com/images/M/MV5BMTI4YTk1ZDEtM2FlZi00N2JhLTgwZmEtM2YxNDk3NjRmZDU1XkEyXkFqcGc@._V1_.jpg", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=x7uLutVRBfI" },
+            { img: "https://ingresso-a.akamaihd.net/b2b/production/uploads/article/image/4380/925876e677ce9c3ba4c58e4ca6b5eeb0.jpg", top10: true, youtube: "https://www.youtube.com/watch?v=AfQ13jsLDms" },
+            { img: "https://sm.ign.com/t/ign_in/feature/a/arcane-sea/arcane-season-2-ending-explained-with-showrunner-christian-l_vnxk.1280.jpg", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=ysqiEC6bLUI" },
+            { img: "https://cdn.ome.lt/G1PSzGUrsDHB6p7VZ9f0APgLP6s=/1200x630/smart/filters:format(webp)/extras/conteudos/all_of_us_final.png", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=IN5TD4VRcSM" },
         ]
     },
     {
